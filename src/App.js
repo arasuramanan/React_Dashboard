002ae8ec58e154dc552d446e67acc0f54fb95381
arasuramanan <arasuaudi@gmail.com>
2023-10-25 09:12:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Dashboard from './components/Dashboard';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import AddUser from './components/AddUser';
+import EditUser from './components/EditUser';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+let data = {
+  earningsMonthly:"4,00,000",
+  earningsAnnual:"48,00,000",
+  task:80,
+  pendingRequest:18
+}
+
+let [users, setUsers] = useState([
+  {
+    name:"LeoDass",
+    email:"Leodass@gmail.com",
+    batch:"B39WDT",
+    mobile:"9442167267"
+  },
+  {
+    name:"KamalHassan",
+    email:"kamalhassan@gmail.com",
+    batch:"B39WDT",
+    mobile:"9444545454"
+  },
+  {
+    name:"Suriya",
+    email:"Suriya1978@gmail.com",
+    batch:"B39WDT",
+    mobile:"9454545477"
+  }
+])
+
+return <>
+
+  <div id='wrapper'>
+    <BrowserRouter>
+    <Sidebar/>
+    <Routes>
+      <Route path='/dashboard' element={<Dashboard data={{data,users,setUsers}}/>}/>
+      <Route path='/add-user' element={<AddUser data={{users,setUsers}}/>}/>
+      <Route path='/edit-user/:id' element={<EditUser data={{users,setUsers}}/>}/>
+      <Route path='*' element={<Navigate to='/add-user'/>}/>
+      
+    </Routes>
+    </BrowserRouter>
     </div>
-  );
+    
+  </> 
 }
 
 export default App;
